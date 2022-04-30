fa298fda2b349c7625b117cdee84fa774f46e41b
Aditya Patil <75464869+Aditya1402@users.noreply.github.com>
2022-05-01 01:04:32 +0530

    Update AppBar

--- day02/lib/palette.dart
+++ day02/lib/palette.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class Palette {
   static const primary = Color(0XFF303030);
   static const secondary = Color(0XFFF0F0F0);
+  static const grey = Color(0xFF808080);
 }
---
+++ day02/lib/widgets/selector.dart
@@ -0,0 +1,44 @@
+import 'package:day02/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Selector extends StatefulWidget {
+  const Selector({Key? key}) : super(key: key);
+
+  @override
+  State<Selector> createState() => _SelectorState();
+}
+
+class _SelectorState extends State<Selector> {
+  bool active = false;
+  int tempIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 65,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    active = !active;
+                    tempIndex = index;
+                  });
+                },
+                child: Container(
+                  child: Icon(FontAwesomeIcons.diamondTurnRight),
+                  width: 64,
+                  decoration: BoxDecoration(
+                      color: tempIndex==index ? Palette.secondary : Palette.primary,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
